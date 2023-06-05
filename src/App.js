a79256d00ed7107cba3e8fa2f6604f45fcb91693
AworThales <thals4jesus@gmail.com>
2023-06-05 05:11:19 -0700

    implementing dark and light mode class

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Profile from './pages/profile/Profile';
 import NavBar from './components/navBar/Navbar'
 import LeftBar from './components/leftBar/LeftBar'
 import RightBar from './components/rightBar/RightBar'
+import './common_style.scss'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -21,7 +22,7 @@ function App() {
 
   const Layout = ()=>{
     return(
-      <div>
+      <div className='theme-dark'>
         <NavBar />
         <div style={{display: "flex"}}>
           <LeftBar />
