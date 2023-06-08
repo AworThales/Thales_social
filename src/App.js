5e99159c9652a3ff23aaa12da722182ffcf6afcd
AworThales <thals4jesus@gmail.com>
2023-06-08 09:32:27 -0700

    current user update

--- src/App.js
+++ src/App.js
@@ -16,13 +16,14 @@ import {
 } from "react-router-dom";
 import { children, useContext } from 'react';
 import { DarkModeContext } from './context/darkModeContext';
+import { AuthContext } from './context/authContext';
 
 function App() {
 
-  const currentUser = true;
+  const {currentUser} = useContext(AuthContext);
 
   const {darkMode} = useContext(DarkModeContext)
-  console.log(darkMode);
+  // console.log(darkMode);
 
   const Layout = ()=>{
     return(
