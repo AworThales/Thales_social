53f5354ad6a81a26d97a967e3e6dbfd0b333c576
AworThales <thals4jesus@gmail.com>
2023-06-10 04:54:15 -0700

    File updated

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -1,39 +1,48 @@
-import React from './stories.scss'
+import { useContext } from "react";
+import "./stories.scss"
+import { AuthContext } from "../../context/authContext"
 
 const Stories = () => {
 
-    // Dummy data
-    const stories = [
-        {
-            id: 1,
-            name: "Thales Awor",
-            img: "https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-          },
-          {
-            id: 2,
-            name: "Thales Solomon",
-            img: "https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-          },
-          {
-            id: 3,
-            name: "Awor Thales",
-            img: "https://images.pexels.com/photos/4371669/pexels-photo-4371669.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-          },
-          {
-            id: 4,
-            name: "Solomon Awor",
-            img: "https://images.pexels.com/photos/7608079/pexels-photo-7608079.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-          },
-    ]
+  const {currentUser} = useContext(AuthContext)
+
+  //TEMPORARY
+  const stories = [
+    {
+      id: 1,
+      name: "Thales Solomon",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "Thales Awor",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      name: "Solomon Thales",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "Awor Thales",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
 
   return (
-    <div className='stories'>
-      {stories.map(story =>{
-        <div className="story">
-            <img src={story.img} alt="" />
-            <span>{story.name}</span>
+    <div className="stories">
+      <div className="story">
+          <img src={currentUser.profilePic} alt="" />
+          <span>{currentUser.name}</span>
+          <button>+</button>
         </div>
-      })}
+      {stories.map(story=>(
+        <div className="story" key={story.id}>
+          <img src={story.img} alt="" />
+          <span>{story.name}</span>
+        </div>
+      ))}
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,48 +1,13 @@
 import React from 'react'
 import './home.scss'
+import Stories from '../../components/stories/Stories'
+import Posts from '../../components/posts/Posts'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
+    <div className='home'>
+      <Stories />
+      <Posts />
     </div>
   )
 }
